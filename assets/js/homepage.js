46c7d03da36584976ac1f79ca62ca34e22b91517
Clayton Goff <cltngoff@gmail.com>
2021-03-03 19:23:42 -0800

    fetch api and error handling complete

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,9 +8,17 @@ var getUserRepos = function(user){
     
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-       response.json().then(function(data) {
+       if (response.ok) {
+        response.json().then(function(data) {
         displayRepos(data, user);
         });
+    } else {
+        alert("Error: " + response.statusText);
+    }
+    })
+    .catch(function(error) {
+        // Notice this `catch()` getting chained oto the `.then()` method
+        alert("Unable to connect to GitHub");
     });
 };
 
@@ -28,6 +36,10 @@ var formSubmitHandler = function(event) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories founds.";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     // clear old content
